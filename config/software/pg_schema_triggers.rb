9bc0a0917e35889dda018a87f9042fc2b2d75b70
Erik Anderson <eanderson20@bloomberg.net>
2015-09-28 16:16:47 -0400

    Fix for build.

--- config/software/pg_schema_triggers.rb
+++ config/software/pg_schema_triggers.rb
@@ -1,8 +1,7 @@
 name 'pg_schema_triggers'
 default_version 'master'
 
-source url: "https://github.com/CartoDB/#{name}/archive/#{version}.tar.gz",
-       md5: '676afc94d2d72185661b84105aa0248e'
+source git: "https://github.com/CartoDB/pg_schema_triggers"
 
 relative_path "#{name}-#{version}"
 
