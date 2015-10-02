9fff5409adda0e37ba07323a46fab5c2b741c2c1
Hugo Sato <hsato42@users.noreply.github.com>
2015-10-02 06:06:43 -0400

    Update cartodb.rb

--- config/software/cartodb.rb
+++ config/software/cartodb.rb
@@ -1,34 +1,20 @@
 name 'cartodb'
 
-# Vishal's branch
-default_version 'blp_sso'
-#default_version 'master'
-
-#source git: "https://github.com/CartoDB/#{name}",
-#       submodules: true
-
-source git: "https://github.com/bloomberg/cartodb",
-       submodules: true
-
-relative_path "#{name}-#{version}"
-
-dependency "cartodb-python-requirements"
+dependency "cartodb-npm"
 
 build do
   env = with_standard_compiler_flags(with_embedded_path)
-
+  
   command 'npm install -g grunt-cli', env: env
   
   # install development and test dependecies for grunt work...
-  command 'npm install -d', env: env
-  command 'npm install grunt-timer', env: env
+  staging_dir = "#{install_dir}/embedded/cartodb"
+  
+  command 'npm install -d',          cwd: staging_dir, env: env
+  command 'npm install grunt-timer', cwd: staging_dir, env: env
 
   command 'grunt', env: env.merge({
      "LC_ALL" => "en_US.UTF-8",
      "LANG" => "en_US.UTF-8"
   })
-  
-  sync "#{project_dir}", "#{install_dir}/embedded/#{name}-#{version}", exclude: ['**/.git', '**/node_modules']
-  
-  link "#{install_dir}/embedded/#{name}-#{version}", "#{install_dir}/embedded/cartodb"
 end
