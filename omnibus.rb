9db6ad94bba3c20bba8e45ffa5ddbc63518a86bd
Hugo Sato <hsato42@users.noreply.github.com>
2015-10-01 21:40:23 -0400

    Update omnibus.rb

--- omnibus.rb
+++ omnibus.rb
@@ -1,14 +1,3 @@
-MYUSERNAME = ENV['LOGNAME']
-
-if MYUSERNAME == 'cartodb'
-  base_dir ENV['OMNIBUS_BASE_DIR'] || '/var/omnibus'
-else
-  base_dir './local'
-  cache_dir     './local/omnibus/cache'
-  git_cache_dir './local/omnibus/cache/git_cache'
-  source_dir    './local/omnibus/src'
-  build_dir     './local/omnibus/build'
-  package_dir   './local/omnibus/pkg'
-end
+base_dir ENV['OMNIBUS_BASE_DIR'] || ( ENV['LOGNAME'] == 'jenkins' ? '/var/lib/jenkins/userContent/omnibus' : './local/omnibus' )
 
 software_gems ['omnibus-software']
