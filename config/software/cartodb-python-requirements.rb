b69c58c8420514c353cdaa58e29bfa1a165da6b7
Hugo Sato <hsato42@users.noreply.github.com>
2015-10-02 12:54:59 -0400

    Update cartodb-python-requirements.rb

--- config/software/cartodb-python-requirements.rb
+++ config/software/cartodb-python-requirements.rb
@@ -7,7 +7,7 @@ build do
   env = with_standard_compiler_flags(with_embedded_path)
   staging_dir = "#{install_dir}/embedded/cartodb"
   command "#{install_dir}/embedded/bin/pip install" \
-          " --build #{build_dir}" \
+          " --build #{build_dir}/#{name}" \
           " --requirement #{install_dir}/embedded/cartodb/python_requirements.txt", cwd: staging_dir, env: env
 #  command "#{install_dir}/embedded/bin/pip install" \
 #          " --build #{build_dir}/#{name}" \
