4bc3a2e676fe035a67e5a664be28de8b682b2e7b
Hugo Sato <hsato42@users.noreply.github.com>
2015-10-02 04:45:33 -0400

    Update cartodb-windshaft.rb

--- config/software/cartodb-windshaft.rb
+++ config/software/cartodb-windshaft.rb
@@ -14,7 +14,6 @@ dependency 'pango'
 build do
   # build from source to link node against embedded mapnik.
   command 'npm install --build-from-source -d', env: with_standard_compiler_flags(with_embedded_path)
-  command 'npm test', env: with_embedded_path
   sync "#{project_dir}", "#{install_dir}/embedded/#{name}-#{version}", exclude: ['**/.git']
   link "#{install_dir}/embedded/#{name}-#{version}", "#{install_dir}/embedded/#{name}"
 end
