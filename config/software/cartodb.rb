58994be0c541dafe2f80fa3c35f96edca22b282d
Hugo Sato <hsato42@users.noreply.github.com>
2015-10-02 02:28:37 -0400

    Update cartodb.rb

--- config/software/cartodb.rb
+++ config/software/cartodb.rb
@@ -14,18 +14,21 @@ relative_path "#{name}-#{version}"
 
 dependency "cartodb-node-modules"
 dependency "cartodb-rubygems"
-dependency "grunt-cli"
 
 build do
   env = with_standard_compiler_flags(with_embedded_path)
 
-  # install devDependecies for grunt run
-  npm = ['npm',
-         'install',
-         '-d',
-         ].join(' ')
-  command npm, env: env
- 
+  # install development and test dependecies for grunt run
+  command 'npm install -d', env: env
+  command 'npm install grunt', env: env
+  command 'npm install timer', env: env
+  command 'bundle install'
+   command bundle, env: env.merge({
+    "BUNDLE_BUILD__CHARLOCK_HOLMES" => "--with-pkg-config=#{install_dir}/embedded/lib/pkgconfig/",
+    "BUNDLE_BUILD__FFI" => "--with-pkg-config=#{install_dir}/embedded/lib/pkgconfig/",
+    "BUNDLE_BUILD__NOKOGIRI" => "--use-system-libraries --with-xml2-lib=#{install_dir}/embedded/lib --with-xml2-include=#{install_dir}/embedded/include/libxml2 --with-xslt-lib=#{install_dir}/embedded/lib --with-xslt-include=#{install_dir}/embedded/include/libxslt --with-iconv-dir=#{install_dir}/embedded --with-zlib-dir=#{install_dir}/embedded"
+  })
+  
   command 'grunt', env: env.merge({
      "LC_ALL" => "en_US.UTF-8",
      "LANG" => "en_US.UTF-8"
