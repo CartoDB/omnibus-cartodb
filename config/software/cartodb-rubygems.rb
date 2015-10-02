41718bfac2ed1786b1c71c604ff5d2b9992410a2
Hugo Sato <hsato42@users.noreply.github.com>
2015-10-02 05:42:07 -0400

    Update cartodb-rubygems.rb

--- config/software/cartodb-rubygems.rb
+++ config/software/cartodb-rubygems.rb
@@ -1,10 +1,5 @@
 name 'cartodb-rubygems'
-# Vishal's branch
 default_version 'blp_sso'
-#default_version 'master'
-
-#source git: "https://github.com/CartoDB/#{name}",
-#       submodules: true
 
 source git: "https://github.com/bloomberg/cartodb",
        submodules: true
@@ -21,16 +16,18 @@ dependency "icu"
 build do
   env = with_standard_compiler_flags(with_embedded_path)
   
+  sync "#{project_dir}", "#{install_dir}/embedded/cartodb-#{version}", exclude: ['**/.git']
+  
   bundle = ["bundle",
             "pack",
             "--all",
-            "--all-platforms",
-            "--path #{install_dir}/embedded/gems",
-            "--gemfile #{project_dir}/Gemfile"
+            "--all-platforms"
             ].join(' ')
-  command bundle, cwd: build_dir, env: env.merge({
+  command bundle, cwd: project_dir, env: env.merge({
     "BUNDLE_BUILD__CHARLOCK_HOLMES" => "--with-pkg-config=#{install_dir}/embedded/lib/pkgconfig/",
     "BUNDLE_BUILD__FFI" => "--with-pkg-config=#{install_dir}/embedded/lib/pkgconfig/",
     "BUNDLE_BUILD__NOKOGIRI" => "--use-system-libraries --with-xml2-lib=#{install_dir}/embedded/lib --with-xml2-include=#{install_dir}/embedded/include/libxml2 --with-xslt-lib=#{install_dir}/embedded/lib --with-xslt-include=#{install_dir}/embedded/include/libxslt --with-iconv-dir=#{install_dir}/embedded --with-zlib-dir=#{install_dir}/embedded"
   })
+  
+  link "#{install_dir}/embedded/#{name}-#{version}", "#{install_dir}/embedded/cartodb
 end
