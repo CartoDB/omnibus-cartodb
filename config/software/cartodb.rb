2da1c97615870f3598c5348c360a9bc14e38b577
Hugo Sato <hsato42@users.noreply.github.com>
2015-10-02 02:57:21 -0400

    Update cartodb.rb

--- config/software/cartodb.rb
+++ config/software/cartodb.rb
@@ -18,16 +18,12 @@ dependency "cartodb-rubygems"
 build do
   env = with_standard_compiler_flags(with_embedded_path)
 
-  # install development and test dependecies for grunt run
-  command 'npm install -d', env: env
-  command 'npm install grunt', env: env
-  command 'npm install grunt-timer', env: env
-  command 'bundle install', env: env.merge({
-    "BUNDLE_BUILD__CHARLOCK_HOLMES" => "--with-pkg-config=#{install_dir}/embedded/lib/pkgconfig/",
-    "BUNDLE_BUILD__FFI" => "--with-pkg-config=#{install_dir}/embedded/lib/pkgconfig/",
-    "BUNDLE_BUILD__NOKOGIRI" => "--use-system-libraries --with-xml2-lib=#{install_dir}/embedded/lib --with-xml2-include=#{install_dir}/embedded/include/libxml2 --with-xslt-lib=#{install_dir}/embedded/lib --with-xslt-include=#{install_dir}/embedded/include/libxslt --with-iconv-dir=#{install_dir}/embedded --with-zlib-dir=#{install_dir}/embedded"
-  })
+  command 'npm install -g grunt', env: env
   
+  # install development and test dependecies for grunt work...
+  command 'npm install -d', env: env
+  command 'npm install grunt-timer', env: env
+
   command 'grunt', env: env.merge({
      "LC_ALL" => "en_US.UTF-8",
      "LANG" => "en_US.UTF-8"
