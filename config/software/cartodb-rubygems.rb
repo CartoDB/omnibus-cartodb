96e3367f4b7df83a28ebcd00766219d61ee7c950
Hugo Sato <hsato42@users.noreply.github.com>
2015-10-02 07:08:41 -0400

    Update cartodb-rubygems.rb

--- config/software/cartodb-rubygems.rb
+++ config/software/cartodb-rubygems.rb
@@ -16,18 +16,20 @@ dependency "icu"
 build do
   env = with_standard_compiler_flags(with_embedded_path)
   
-  sync "#{project_dir}", "#{install_dir}/embedded/cartodb-#{version}", exclude: ['**/.git']
+  staging_dir = "#{install_dir}/embedded/cartodb-#{version}"
+  sync "#{project_dir}",staging_dir, exclude: ['**/.git']
   
   bundle = ["bundle",
             "pack",
             "--all",
             "--all-platforms"
             ].join(' ')
-  command bundle, cwd: project_dir, env: env.merge({
+  command bundle, cwd: staging_dir, env: env.merge({
     "BUNDLE_BUILD__CHARLOCK_HOLMES" => "--with-pkg-config=#{install_dir}/embedded/lib/pkgconfig/",
     "BUNDLE_BUILD__FFI" => "--with-pkg-config=#{install_dir}/embedded/lib/pkgconfig/",
     "BUNDLE_BUILD__NOKOGIRI" => "--use-system-libraries --with-xml2-lib=#{install_dir}/embedded/lib --with-xml2-include=#{install_dir}/embedded/include/libxml2 --with-xslt-lib=#{install_dir}/embedded/lib --with-xslt-include=#{install_dir}/embedded/include/libxslt --with-iconv-dir=#{install_dir}/embedded --with-zlib-dir=#{install_dir}/embedded"
   })
   
-  link "#{install_dir}/embedded/#{name}-#{version}", "#{install_dir}/embedded/cartodb"
+  # this will make it easy for dependents to not care about the version suffix
+  link staging_dir, "#{install_dir}/embedded/cartodb"
 end
