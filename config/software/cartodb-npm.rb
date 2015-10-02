befec55a2484f25fed76b42edaecf1204fab6edf
Hugo Sato <hsato42@users.noreply.github.com>
2015-10-02 08:12:29 -0400

    Update cartodb-npm.rb

--- config/software/cartodb-npm.rb
+++ config/software/cartodb-npm.rb
@@ -6,5 +6,5 @@ dependency 'cartodb-python-requirements'
 build do
   env = with_standard_compiler_flags(with_embedded_path)
   staging_dir = "#{install_dir}/embedded/cartodb"
-  command 'npm install -d', cwd: staging_dir, env: env
+  command 'npm install --build-from-source -d', cwd: staging_dir, env: env
 end
