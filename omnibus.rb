4bd7a019aff5b2a7747aac7ef0d201e581efc0b4
Hugo Sato <hsato42@users.noreply.github.com>
2015-10-05 17:09:36 -0400

    Update omnibus.rb

--- omnibus.rb
+++ omnibus.rb
@@ -1,3 +1,6 @@
 base_dir ENV['OMNIBUS_BASE_DIR'] || ( ENV['LOGNAME'] == 'jenkins' ? '/var/lib/jenkins/userContent/omnibus' : './local/omnibus' )
 
 software_gems ['omnibus-software']
+
+# commands that download resources from Internet like `npm install` and `bundle` may timeout when running behind a proxy
+build_retries 3
