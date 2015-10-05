3cc26848f90761e4b4dec489cfe8c29654b70d65
Hugo Sato <hsato42@users.noreply.github.com>
2015-10-05 14:47:10 -0400

    Update cartodb-python-requirements.rb

--- config/software/cartodb-python-requirements.rb
+++ config/software/cartodb-python-requirements.rb
@@ -9,14 +9,4 @@ build do
   command "#{install_dir}/embedded/bin/pip install" \
           " --build #{build_dir}/#{name}" \
           " --requirement #{install_dir}/embedded/cartodb/python_requirements.txt", cwd: staging_dir, env: env
-#  command "#{install_dir}/embedded/bin/pip install" \
-#          " --build #{build_dir}/#{name}" \
-#          " chardet==2.3.0" \
-#          " argparse==1.2.1" \
-#          " brewery==0.6" \
-#          " redis==2.4.9" \
-#          " python-varnish==0.1.2" \
-#          " csvkit==0.9.0" \
-#          " openpyxl==2.1.3" \
-#          " gdal==2.0.1", cwd: staging_dir, env: env
 end
