97b9a4275ea7221023fd888266a9efae8c5381ba
Hugo Sato <hsato42@users.noreply.github.com>
2015-10-06 16:58:55 -0400

    Update cartodb-infrastructure.rb

--- config/projects/cartodb-infrastructure.rb
+++ config/projects/cartodb-infrastructure.rb
@@ -20,72 +20,25 @@ install_dir "#{package_root}/#{name}"
 exclude "**/.git"
 exclude "**/bundler/git"
 
-#gdal dependencies
-dependency 'libiconv'
-dependency 'zlib'
-dependency 'expat'
-dependency 'curl'
-dependency 'freexl'
-dependency 'geos'
-dependency 'json-c'
-dependency 'libgeotiff'
-dependency 'libjpeg'
-dependency 'liblzma'
-dependency 'libpng'
-dependency 'libtiff'
-dependency 'libxml2'
-dependency 'postgresql'
-dependency 'python'
-dependency 'ruby'
-dependency 'sqlite'
-dependency 'libwebp'
-dependency 'zip'
-
-# general dependency
-dependency 'nodejs'
-
-# Postgis dependencies
-dependency 'postgresql'
-dependency 'postgis'
-dependency "libossp-uuid" 
-
-dependency "zlib"
-dependency "curl"
-dependency "cairo"
-dependency "freetype"
-dependency "harfbuzz"
-dependency "icu"
-dependency "json-c"
-dependency "libjpeg"
-dependency "libpng"
-dependency "libtiff"
-dependency "libffi"
-dependency "libxml2"
-dependency "boost"
-dependency "sqlite"
-dependency "proj"
-dependency "python"
-dependency "python-nose"
-dependency "ruby"
-
-# remove this from the root package
-dependency "gdal"
-dependency "gdalautotest"
-
 dependency "preparation"
+
 dependency "cacerts"
-dependency "unicorn"
-dependency "imagemagick"
-dependency "redis"
-dependency "unp"
-dependency "cartodb-python-requirements"
-dependency "varnish-cache"
-dependency "forever"
-dependency "version-manifest"
-dependency "ruby-saml-gem"
+dependency "postgis"
 dependency "pg-gem"
-dependency "zip"
+
+dependency "cartodb-gdal"
+
+dependency "cartodb-mapnik"
+
+dependency "redis"
+dependency "varnish-cache"
+
 dependency "ruby"
 dependency "bundler"
-dependency "nokogiri"
+dependency "ruby-saml-gem"
+dependency "unicorn"
+
 dependency 'nodejs'
+dependency "forever"
+
+dependency "version-manifest"
