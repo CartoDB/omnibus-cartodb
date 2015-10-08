8e605a977628e64f56621337fdddb73e15dd7bc1
Hugo Sato <hsato42@users.noreply.github.com>
2015-10-08 19:36:53 -0400

    Create cartodb-infrastructure.rb

---
+++ config/software/cartodb-infrastructure.rb
@@ -0,0 +1,29 @@
+name 'cartodb-infrastructure'
+
+# language tool-chains
+dependency 'ruby'
+dependency 'bundler'
+dependency 'nokogiri'
+dependency 'python'
+dependency 'pip'
+dependency 'python-nose'
+dependency 'nodejs'
+dependency 'phantomjs'
+dependency 'grunt-cli'
+dependency 'perl'
+dependency 'protobuf'
+
+# data storage and retrieval
+dependency 'sqlite'
+dependency 'redis'
+dependency 'postgis'
+dependency 'varnish-cache'
+
+# cartodb customized applications
+dependency 'cartodb-gdal'
+dependency 'cartodb-mapnik'
+
+# 3rd party applications
+dependency 'imagemagick'
+dependency 'unicorn'
+dependency 'zip'
