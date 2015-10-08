78ffb226aed874c0a6ed06312b0cb6f8ac87de0e
Hugo Sato <hsato42@users.noreply.github.com>
2015-10-08 19:36:24 -0400

    Update cartodb-infrastructure.rb

--- config/projects/cartodb-infrastructure.rb
+++ config/projects/cartodb-infrastructure.rb
@@ -21,36 +21,6 @@ exclude "**/.git"
 exclude "**/bundler/git"
 
 dependency "preparation"
-
-# layer 0
 dependency 'cartodb-layer0-devel'
-
-# language tool-chains
-dependency 'ruby'
-dependency 'bundler'
-dependency 'nokogiri'
-dependency 'python'
-dependency 'pip'
-dependency 'python-nose'
-dependency 'nodejs'
-dependency 'phantomjs'
-dependency 'grunt-cli'
-dependency 'perl'
-dependency 'protobuf'
-
-# data storage and retrieval
-dependency 'sqlite'
-dependency 'redis'
-dependency 'postgis'
-dependency 'varnish-cache'
-
-# cartodb customized applications
-dependency 'cartodb-gdal'
-dependency 'cartodb-mapnik'
-
-# 3rd party applications
-dependency 'imagemagick'
-dependency 'unicorn'
-dependency 'zip'
-
+dependency 'cartodb-infrastructure'
 dependency "version-manifest"
