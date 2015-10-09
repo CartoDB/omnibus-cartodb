d133df8ac1cea359ec0fbe5753b35020df738379
Hugo Sato <hsato42@users.noreply.github.com>
2015-10-09 07:29:10 -0400

    Update cartodb-windshaft.rb

--- config/software/cartodb-windshaft.rb
+++ config/software/cartodb-windshaft.rb
@@ -4,13 +4,6 @@ default_version 'cdb'
 source git: "https://github.com/CartoDB/Windshaft-cartodb"
 relative_path "#{name}-#{version}"
 
-dependency 'libXrender'
-# depends on mapnik not node-mapnik b/c windshaft-cartodb/package.json already pulls node-mapnik
-dependency 'cartodb-mapnik'
-dependency 'nodejs'
-dependency 'protobuf'
-dependency 'pango'
-
 build do
   # build from source to link node against embedded mapnik.
   command 'npm install --build-from-source -d', env: with_standard_compiler_flags(with_embedded_path)
