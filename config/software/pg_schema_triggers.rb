f796fb35125de917d27129ba464896a47949d699
Hugo Sato <hsato42@users.noreply.github.com>
2015-10-09 07:49:01 -0400

    Update pg_schema_triggers.rb

--- config/software/pg_schema_triggers.rb
+++ config/software/pg_schema_triggers.rb
@@ -5,8 +5,6 @@ source git: "https://github.com/CartoDB/pg_schema_triggers"
 
 relative_path "#{name}-#{version}"
 
-dependency "cartodb-postgis"
-
 build do
   env = with_standard_compiler_flags(with_embedded_path)
   make "-j #{workers}", env: env
