afb4821712d804f0f0a9d2c9fbe73ec684d68415
Hugo Sato <hsato42@users.noreply.github.com>
2015-10-09 07:22:49 -0400

    Update libwebp.rb

--- config/software/libwebp.rb
+++ config/software/libwebp.rb
@@ -3,10 +3,7 @@ default_version '0.4.3'
 
 source git: "https://chromium.googlesource.com/webm/#{name}"
 
-#source url: "http://downloads.webmproject.org/releases/#{name}/lib#{name}-#{version}.tar.gz",
-#       md5: '08813525eeeffe7e305b4cbfade8ae9b'
-
-relative_path "#{name}"
+relative_path "#{name}-#{version}"
 
 dependency 'libpng'
 dependency 'libjpeg'
