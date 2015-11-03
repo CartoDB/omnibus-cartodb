8f4ddac3be408e4b73fe5800f719499d11284185
Hugo Sato <hsato42@users.noreply.github.com>
2015-11-03 13:55:05 -0500

    Update and rename ruby-net-scp-gem.rb to ruby-net-sftp-gem.rb

--- config/software/ruby-net-sftp-gem.rb
+++ config/software/ruby-net-sftp-gem.rb
@@ -1,5 +1,5 @@
-name "ruby-net-scp-gem"
-default_version "1.2.1"
+name "ruby-net-sftp-gem"
+default_version "2.1.2"
 
 dependency "ruby"
 dependency "rubygems"
@@ -7,7 +7,7 @@ dependency "rubygems"
 build do
   env = with_standard_compiler_flags(with_embedded_path)
 
-  gem "install net-scp" \
+  gem "install net-sftp" \
       " --version '#{version}'" \
       " --bindir '#{install_dir}/embedded/bin'" \
       " --no-ri --no-rdoc", env: env
