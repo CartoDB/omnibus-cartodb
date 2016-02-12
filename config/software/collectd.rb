00e9bf35fc1425762cacd3e4f7a37fb9a4e1e928
Hugo Sato <hsato42@users.noreply.github.com>
2016-02-12 16:31:12 -0500

    Create collectd.rb

---
+++ config/software/collectd.rb
@@ -0,0 +1,17 @@
+name 'collectd'
+default_version '5.5.0'
+
+source git: "https://github.com/collectd/collectd.git"
+
+relative_path "#{name}-#{version}"
+
+build do
+  command "./build.sh", env: with_standard_compiler_flags(with_embedded_path)
+
+  configure = ['./configure',
+               "--prefix=#{install_dir}/embedded"
+              ].join(' ')
+  command configure, env: with_standard_compiler_flags(with_embedded_path)
+  make "-j #{workers}", env: with_standard_compiler_flags(with_embedded_path)
+  make 'install'
+end
