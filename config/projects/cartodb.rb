2a2301ebbe4141ffbd2cf69887c972364d00e1fb
Hugo Sato <hsato42@users.noreply.github.com>
2016-03-01 13:57:47 -0500

    Update cartodb.rb

--- config/projects/cartodb.rb
+++ config/projects/cartodb.rb
@@ -19,6 +19,8 @@ install_dir "#{package_root}/#{name}"
 exclude "**/.git"
 exclude "**/bundler/git"
 
+override :ruby, version: "2.2.3"
+
 dependency "preparation"
 dependency "cartodb-infrastructure"
 dependency "cartodb-editor"
