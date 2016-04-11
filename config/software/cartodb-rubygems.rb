c2284aa112a144816b15e08d02f3d821a7e4d8a3
Vishal Jadhav <vishalya@users.noreply.github.com>
2016-04-11 12:32:45 -0400

    to flush the cache

--- config/software/cartodb-rubygems.rb
+++ config/software/cartodb-rubygems.rb
@@ -15,7 +15,7 @@ build do
   sync "#{project_dir}", staging_dir
   
   bundle = ["bundle",
-            "install",
+            "install ",
             "--path=#{install_dir}/embedded/gem",
             "--binstubs=#{install_dir}/embedded/bin",
             "--deployment",
@@ -32,7 +32,7 @@ build do
   command 'npm install -d',          cwd: staging_dir, env: env
   command 'npm install grunt-timer', cwd: staging_dir, env: env
 
-  command 'grunt ', cwd: staging_dir, env: env.merge({
+  command 'grunt', cwd: staging_dir, env: env.merge({
      "LC_ALL" => "en_US.UTF-8",
      "LANG" => "en_US.UTF-8"
   })
