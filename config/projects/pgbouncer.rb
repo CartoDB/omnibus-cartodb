b1becf9a1e6e2053b3ab24a435cb372bfb2ec292
Hugo Sato <hsato42@users.noreply.github.com>
2016-08-03 11:09:57 -0400

    Create pgbouncer.rb

---
+++ config/projects/pgbouncer.rb
@@ -0,0 +1,25 @@
+# see https://github.com/chef/omnibus/blob/master/lib/omnibus/project.rb for more detailed class doc
+name "pgbouncer"
+maintainer "CartoDB team"
+homepage "https://github.com/CartoDB/omnibus-cartodb/blob/master/README.md"
+
+build_version Omnibus::BuildVersion.semver
+
+package_user   ENV['OMNIBUS_PACKAGE_USER']  || ENV['LOGNAME'] || 'root'
+package_group  ENV['OMNIBUS_PACKAGE_GROUP'] || ENV['OMNIBUS_PACKAGE_USER'] || ENV['LOGNAME'] || 'root'
+package_root = ENV['OMNIBUS_PACKAGE_ROOT']  || '/opt'
+
+build_iteration ENV['OMNIBUS_PROJECT_BUILD_ITERATION'] || 1
+
+install_dir "#{package_root}/#{name}"
+
+# .rpm and .deb are already compressed so the below compress doesnt help much. 
+# compress :tgz
+
+exclude "**/.git"
+exclude "**/bundler/git"
+
+dependency "preparation"
+dependency 'repmgr'
+dependency 'pgbouncer'
+dependency "version-manifest"
