98de1dacc2defd23f0f462f75ebef302176a2603
akatuluru <akatuluru@bloomberg.net>
2016-09-20 19:52:58 -0400

    Update postgresql.rb

--- config/software/postgresql.rb
+++ config/software/postgresql.rb
@@ -71,9 +71,9 @@ version "9.5.3" do
   source md5: "3f0c388566c688c82b01a0edf1e6b7a0"
 end
 
-version "9.5" do
-  source md5: "7e06af1fce2325a737deffb44e538e32"
-end
+#version "9.5" do
+ # source md5: "7e06af1fce2325a737deffb44e538e32"
+#end
 #source url: "http://ftp.postgresql.org/pub/source/v#{version}/postgresql-#{version}.tar.bz2"
 #source url: "https://github.com/CartoDB/postgres/archive/REL9_5_STABLE_extension_shipping.zip"
 source git: "bbgithub:datavis-cartodb/postgres9.5fdw"
