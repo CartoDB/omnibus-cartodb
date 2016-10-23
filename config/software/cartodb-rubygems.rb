b28d740c947263d81b393ce5e8bc04a62b6e64c6
Hugo Sato <hsato42@users.noreply.github.com>
2016-10-23 17:16:17 -0400

    Update cartodb-rubygems.rb

--- config/software/cartodb-rubygems.rb
+++ config/software/cartodb-rubygems.rb
@@ -1,9 +1,8 @@
 name 'cartodb-rubygems'
-default_version 'blp_prod'
+default_version 'blp_dev'
 
 source git: "https://github.com/bloomberg/cartodb",    
        submodules: true
-
 
 relative_path "#{name}-#{version}"
 
