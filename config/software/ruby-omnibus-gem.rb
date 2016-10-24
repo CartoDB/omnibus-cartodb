f6cb96083a08976070774616cf59c28f8fe3e1ae
Hugo Sato <hsato42@users.noreply.github.com>
2016-10-23 23:25:43 -0400

    Update ruby-omnibus-gem.rb

--- config/software/ruby-omnibus-gem.rb
+++ config/software/ruby-omnibus-gem.rb
@@ -8,16 +8,17 @@ dependency "rubygems"
 
 build do
   env = with_standard_compiler_flags(with_embedded_path)
-  
+
+  staging_dir = "#{install_dir}/embedded/omnibus-cartodb-#{version}"
+  sync "#{project_dir}", staging_dir
+
   bundle = ["bundle",
             "install",
             "--path=#{install_dir}/embedded/gem",
-            "--binstubs=#{install_dir}/bin",
-            "--deployment",
-            "--retry 4",
+            "--binstubs=#{install_dir}/embedded/bin",
             "-j#{workers}"
             ].join(' ')
-            
+
   command bundle, cwd: staging_dir, env: env.merge({
     "BUNDLE_BUILD__CHARLOCK_HOLMES" => "--with-pkg-config=#{install_dir}/embedded/lib/pkgconfig/ ",
     "BUNDLE_BUILD__FFI" => "--with-pkg-config=#{install_dir}/embedded/lib/pkgconfig/",
