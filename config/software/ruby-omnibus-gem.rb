ee6dd900406e462ee192fd6edd2d05c409a0e1d0
Hugo Sato <hsato42@users.noreply.github.com>
2016-10-23 22:02:33 -0400

    Create ruby-omnibus-gem.rb

---
+++ config/software/ruby-omnibus-gem.rb
@@ -0,0 +1,26 @@
+name ruby-omnibus-gem
+default_version "master"
+
+source git: "https://github.com/cartodb/omnibus-cartodb"
+
+dependency "ruby"
+dependency "rubygems"
+
+build do
+  env = with_standard_compiler_flags(with_embedded_path)
+  
+  bundle = ["bundle",
+            "install",
+            "--path=#{install_dir}/embedded/gem",
+            "--binstubs=#{install_dir}/bin",
+            "--deployment",
+            "--retry 4",
+            "-j#{workers}"
+            ].join(' ')
+            
+  command bundle, cwd: staging_dir, env: env.merge({
+    "BUNDLE_BUILD__CHARLOCK_HOLMES" => "--with-pkg-config=#{install_dir}/embedded/lib/pkgconfig/ ",
+    "BUNDLE_BUILD__FFI" => "--with-pkg-config=#{install_dir}/embedded/lib/pkgconfig/",
+    "BUNDLE_BUILD__NOKOGIRI" => "--use-system-libraries --with-xml2-lib=#{install_dir}/embedded/lib --with-xml2-include=#{install_dir}/embedded/include/libxml2 --with-xslt-lib=#{install_dir}/embedded/lib --with-xslt-include=#{install_dir}/embedded/include/libxslt --with-iconv-dir=#{install_dir}/embedded --with-zlib-dir=#{install_dir}/embedded"
+  })
+end
