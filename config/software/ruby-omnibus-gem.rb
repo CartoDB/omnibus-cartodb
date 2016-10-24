a82db616c803f4600ed29d62bba4c374e2f09595
Hugo Sato <hsato42@users.noreply.github.com>
2016-10-23 23:34:57 -0400

    Blp dev (#64)
    * Update cartodb-rubygems.rb
    
    * Update boost.rb
    
    * Update boost.rb
    
    * Update boost.rb
    
    * Update glib2.rb
    
    * Update cacerts.rb
    
    * Update ruby.rb
    
    * Create ruby-omnibus-gem.rb
    
    * Rename ruby.rb to omnibus-cartodb.rb
    
    * Update ruby-omnibus-gem.rb
    
    * Update libspectre.rb
    
    * Update omnibus-cartodb.rb
    
    * Update ruby-omnibus-gem.rb

---
+++ config/software/ruby-omnibus-gem.rb
@@ -0,0 +1,27 @@
+name "ruby-omnibus-gem"
+default_version "master"
+
+source git: "https://github.com/cartodb/omnibus-cartodb"
+
+dependency "ruby"
+dependency "rubygems"
+
+build do
+  env = with_standard_compiler_flags(with_embedded_path)
+
+  staging_dir = "#{install_dir}/embedded/omnibus-cartodb-#{version}"
+  sync "#{project_dir}", staging_dir
+
+  bundle = ["bundle",
+            "install",
+            "--path=#{install_dir}/embedded/gem",
+            "--binstubs=#{install_dir}/embedded/bin",
+            "-j#{workers}"
+            ].join(' ')
+
+  command bundle, cwd: staging_dir, env: env.merge({
+    "BUNDLE_BUILD__CHARLOCK_HOLMES" => "--with-pkg-config=#{install_dir}/embedded/lib/pkgconfig/ ",
+    "BUNDLE_BUILD__FFI" => "--with-pkg-config=#{install_dir}/embedded/lib/pkgconfig/",
+    "BUNDLE_BUILD__NOKOGIRI" => "--use-system-libraries --with-xml2-lib=#{install_dir}/embedded/lib --with-xml2-include=#{install_dir}/embedded/include/libxml2 --with-xslt-lib=#{install_dir}/embedded/lib --with-xslt-include=#{install_dir}/embedded/include/libxslt --with-iconv-dir=#{install_dir}/embedded --with-zlib-dir=#{install_dir}/embedded"
+  })
+end
