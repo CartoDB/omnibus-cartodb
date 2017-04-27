aa4bae20d308d4fa0f1c1011b7490a0e2ea030bc
Guido Fioravantti <guido.fioravantti@gmail.com>
2017-04-27 12:16:38 +0000

    update version

--- config/software/glib2.rb
+++ config/software/glib2.rb
@@ -1,5 +1,9 @@
 name "glib2"
-default_version "2.52.0"
+default_version "2.50.3"
+
+version "2.50.3" do
+  source md5: "410d41db542fdcd600ca90cf5100ab6f"
+end
 
 version "2.42.0" do
   source md5: "dc08133ca0e4ffd61bb626e8bc35eb3c"
@@ -11,10 +15,6 @@ end
 
 version "2.49.1" do
   source md5: "6012cb3e39325bccc7456a3fa13ed28e"
-end
-
-version "2.52.0" do
-  source md5: "c427b3fd73cff7875a471de3f8a70286"
 end
 
 dependency "gettext"
@@ -42,11 +42,6 @@ build do
 	"--with-pcre=system",    
 	"--with-libiconv",
   ]
-
-  # We dont have a libmount omnibus package so disable the build option for now.
-  if version == "2.52.0"
-    configure.push("--enable-libmount=no")
-  end
 
   command configure.join(" "), :env => env
   command "make -j #{workers}", :env => env
