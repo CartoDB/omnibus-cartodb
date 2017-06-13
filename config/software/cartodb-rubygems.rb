38b2c7e8284e514f1fc767bc94a8d05f3d769d18
Vishal Jadhav <vishalya@users.noreply.github.com>
2017-06-13 15:52:32 -0400

    get only the python_requirements

--- config/software/cartodb-rubygems.rb
+++ config/software/cartodb-rubygems.rb
@@ -1,11 +1,18 @@
 name 'cartodb-rubygems'
-default_version 'blp_prod'
+default_version 'blp_dev'
+
+source git: "https://github.com/bloomberg/cartodb",    
+       submodules: true
 
 relative_path "#{name}-#{version}"
 
 build do
   staging_dir = "#{install_dir}/embedded/cartodb-#{version}"
   sync "#{project_dir}", staging_dir
+
+  delete "#{staging_dir}/*/"
+  delete "#{staging_dir}/!(python_requirements.txt)"
+  
   # this will make it easy for dependents to not care about the version suffix
   link staging_dir, "#{install_dir}/embedded/cartodb"
 end
