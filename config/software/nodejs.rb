e990bdcca8be129946cd6ff6a0668074431dd0ad
Vinayak <vmanohar1@users.noreply.github.com>
2017-10-30 14:48:18 -0400

    Update nodejs.rb

--- config/software/nodejs.rb
+++ config/software/nodejs.rb
@@ -1,5 +1,5 @@
 name 'nodejs'
-default_version '0.10.48'
+default_version '6.9.5'
 
 dependency "python"
 
@@ -25,6 +25,10 @@ end
 
 version "0.10.48" do
   source md5: '6646738d664ca4908ad0f288498c2334'
+end
+
+version "6.9.5" do
+  source md5: 'a2a820b797fb69ffb259b479c7f5df32'
 end
 
 #### version 0.11 is not yet deemed stable, we shouldn't be shipping it
