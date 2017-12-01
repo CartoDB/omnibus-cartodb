8813c08bf36797ebf37ee0018e690e4cc34c163c
root <root@ip-1-70-237-102.dob1.bcpc.bloomberg.com>
2017-12-01 09:55:45 -0500

    postgis version change

--- config/software/cartodb-postgis.rb
+++ config/software/cartodb-postgis.rb
@@ -1,8 +1,10 @@
 name 'cartodb-postgis'
-default_version '2.2.2'
+#default_version '2.2.2'
+default_version '2.4.2'
 
 source url: "http://download.osgeo.org/postgis/source/postgis-#{version}.tar.gz",
-       md5: 'ed42ad50405520bc1210bd4d3a872617'
+       md5: 'b6eebbb708cb0a67bbbe8edc9152780c'
+#md5: 'ed42ad50405520bc1210bd4d3a872617'
 
 relative_path "postgis-#{version}"
 
