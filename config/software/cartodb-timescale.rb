71af22706cb975dd5b5fb5ae64963fb1d95240c0
vmanohar <vmanohar1@bloomberg.net>
2017-12-03 15:51:36 -0500

    syntax correction

--- config/software/cartodb-timescale.rb
+++ config/software/cartodb-timescale.rb
@@ -1,7 +1,7 @@
-name 'cartodb-timescale'
+name 'cartodb-timescaledb'
 default_version '0.7.1'
 
-source url: "https://github.com/timescale/timescaledb/archive/#{version}.tar.gz"
+source url: "https://github.com/timescale/timescaledb/archive/#{version}.tar.gz",
        md5: '26333871d58e6d0039dedce256afd595'
 
 relative_path "timescaledb-#{version}"
