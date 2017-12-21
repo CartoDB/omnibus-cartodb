4423b196f7d0caeeac4f4d02a162c966b04f2e83
Vishal Jadhav <vishalya@users.noreply.github.com>
2017-12-21 15:58:46 -0500

    Update cartodb-mapnik.rb

--- config/software/cartodb-mapnik.rb
+++ config/software/cartodb-mapnik.rb
@@ -2,7 +2,8 @@ name 'cartodb-mapnik'
 #default_version 'a22b3e54e885ccea404ba5997a052152e39b7d94'   # required for static maps export
 default_version 'v3.0.11'
 
-source git: 'https://github.com/mapnik/mapnik.git'
+source git: 'https://github.com/mapnik/mapnik.git',
+             submodules: true
 
 relative_path "#{name}-#{version}"
 
