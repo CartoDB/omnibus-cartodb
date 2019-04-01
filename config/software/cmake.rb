8f9693a2feea617f908743ecb9dbe724eb9e95d9
Vinayak Manohar <vmanohar@ip-1-70-99-15.dob1.bcpc.bloomberg.com>
2019-04-01 08:14:57 -0400

    Remote DB build for 9.6.8 version with timescale version using latest master changes

---
+++ config/software/cmake.rb
@@ -0,0 +1,28 @@
+#
+# Copyright 2012-2014 Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+name "cmake"
+default_version "3.10.0"
+
+source url: "https://cmake.org/files/v3.10/cmake-3.10.0.tar.gz",
+       md5:  "f3f8e70ca3055f3cd288f89ff233057e"
+       
+relative_path "#{name}-#{version}"
+
+build do
+  env = with_standard_compiler_flags(with_embedded_path)
+  command "./bootstrap --prefix=/bb/datavis/cartodb/embedded && make && make install", env:env 
+end
