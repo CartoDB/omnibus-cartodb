429aed5f9b938788e2e0ce5041e408d873626b5f
Vinayak Manohar <vmanohar@ip-1-70-99-15.dob1.bcpc.bloomberg.com>
2019-09-06 11:32:36 -0400

    V1.16.0

--- config/software/bundler.rb
+++ config/software/bundler.rb
@@ -3,5 +3,5 @@ dependency "rubygems"
 
 build do
   env = with_standard_compiler_flags(with_embedded_path)
-  command "#{install_dir}/embedded/bin/gem install bundler --no-ri --no-rdoc", env: env
+  command "#{install_dir}/embedded/bin/gem install bundler -v 1.16.0 --no-ri --no-rdoc", env: env
 end
